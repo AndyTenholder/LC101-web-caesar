da4609fe52beb7aff2ad2093151796f6afa53a6b
Andy Tenholder <andytenholder@gmail.com>
2017-09-25 16:51:50 -0500

    Put HTML in Jinja template

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from caesar import rot13
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
-form = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-    </head>
-    <body>
-        <form action="/" method="get">
-            <label for="rotation">Rotate by:</label>
-            <input id="rotation" type="text" name="rot" value="0"/>
-            <textarea id="text_to_encrypt" name="text" rows="10" cols="50">{0}</textarea>
-            <input type="submit" value ="encrypt using GET"/>
-            <input type="submit" formmethod="post" formaction="/" value='encrypt using POST'/>
-        </form>
-    </body>
-
-</html>
-"""
 
 @app.route("/", methods=['GET'])
 def index():
@@ -45,7 +13,8 @@ def index():
         rot = int(rot)
         message = request.args.get('text')
         encrypted_message = rot13(message,rot)
-    return form.format(encrypted_message)
+        
+    return render_template('web_caesar.html', text = encrypted_message)
 
 @app.route("/" , methods=['POST'])
 def encrypt():
@@ -54,6 +23,6 @@ def encrypt():
     message = request.form['text']
     encrypted_message = rot13(message,rot)
 
-    return form.format(encrypted_message)
+    return render_template('web_caesar.html', text = encrypted_message)
 
 app.run()
